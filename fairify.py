75862c36110aa83133f11902a327e84b271edbab
Raiaku <genuresmith@gmail.com>
2019-07-09 13:45:47 -0400

    add docstrings for 3 new functions

--- fairify.py
+++ fairify.py
@@ -62,4 +62,39 @@ def get_highest_contributor(contributor_dict):
         if len(playlist_items) > most_songs_added:
             highest_contributor = contributor
             most_songs_added = len(playlist_items)
-    return(highest_contributor, most_songs_added)
+    return highest_contributor, most_songs_added
+
+
+def fairify_duplication(contributors_dict):
+    """duplicate tracks so that each contributor has the same amount of items
+    
+    :param contributors_dict: dictionary of playlist items separated by contributors
+    :type contributors_dict: dict
+    :returns: a dictionary of tracks equally provided from each contributor 
+    :rtype: dict
+    """
+    
+    for contributor, playlist_items in contributor_dict.items():
+
+
+def fairify_round_robin(contributors_dict):
+    """round robin sort track items using one track form each contributor, this will be
+    repeated until all track items have been added to the list
+    
+    :param contributors_dict: dictionary of tracks equally provided from each contributor
+    :type contributors_dict: dict
+    :returns: list of songs fairly shuffled by contributors
+    :rtype: list
+    """
+
+
+def fairify_create_playlist(name, track_list):
+    """create a playlist using a list of tracks
+    
+    :param name: name of playlist to create
+    :type name: string
+    :param track_list: tracks to be added playlist
+    :type track_list: list
+    :returns: named playlist with tracks added 
+    :rtype: SpotiwisePlaylist
+    """
